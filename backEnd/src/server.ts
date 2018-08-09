7713a63d10d283be304298227ec3296c22f0031f
neriyarosner <neriyarosner@gmail.com>
2018-08-09 12:51:26 +0300

    console output fix

--- backEnd/src/server.ts
+++ backEnd/src/server.ts
@@ -11,7 +11,7 @@ app.use(errorHandler());
  * Start Express server.
  */
 const server = app.listen(app.get("port"), app.get("host"), () => {
-  console.log(("  App is running at http://localhost:%d in %s mode"), app.get("port"), app.get("env"));
+  console.log(("  App is running at http://%s:%d in %s mode"), app.get("host"), app.get("port"), app.get("env"));
   console.log("  Press CTRL-C to stop\n");
 });
 
