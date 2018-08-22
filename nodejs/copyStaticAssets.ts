0e4a72de0a161aae6aa4056bf554f77ab63600a4
neriyarosner <neriyarosner@gmail.com>
2018-08-22 19:56:51 +0300

    rename backend folder to nodejs, rename frontEnd folder to nativescript

--- nodejs/copyStaticAssets.ts
+++ nodejs/copyStaticAssets.ts
@@ -5,6 +5,6 @@ shell.cp("-R", "src/public/js/lib", "dist/public/js/");
 shell.cp("-R", "src/public/fonts", "dist/public/");
 shell.cp("-R", "src/public/images", "dist/public/");
 
-shell.cp("-R", "../frontEnd/src/assets", "dist/public/assets");
+shell.cp("-R", "../angular/src/assets", "dist/public/assets");
 
-shell.exec("cd ..\\frontEnd && ng build --watch --aot --output-path ..\\backEnd\\dist\\app && cd ..\\backEnd", );
+shell.exec("cd ..\\angular && ng build --watch --aot --output-path ..\\nodejs\\dist\\app && cd ..\\nodejs", );
