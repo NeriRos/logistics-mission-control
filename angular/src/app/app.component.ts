d3acfe121373e759759c1a21a0d8ebc0e702161d
neriyarosner <neriyarosner@gmail.com>
2018-10-31 21:34:16 +0200

    Revert "add user details"
    This reverts commit 1a7cf9129053408028e0f13d5fd08adf8c9fdfc0.
    
    # Conflicts:
    #	angular/src/app/app.component.html
    #	angular/src/app/app.component.ts

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -4,10 +4,11 @@ import { IUser, USER_PERMISSIONS } from "./models/user.model";
 import { MenuComponent } from "./menu/menu.component";
 
 @Component({
-    selector: "app-root",
-    templateUrl: "./app.component.html",
-    styleUrls: ["./app.component.css"]
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
+<<<<<<< HEAD
 export class AppComponent extends MenuComponent implements OnInit {
     protected user: IUser;
 
@@ -33,4 +34,51 @@ export class AppComponent extends MenuComponent implements OnInit {
             return regex.test($(elem)[attr.method](attr.property));
         };
     }
+=======
+export class AppComponent implements OnInit {
+  user: IUser;
+  menus: Array<{name: string, link: string, permission: number}> = [];
+
+  constructor(private userService: UserService) {
+
+  }
+
+  ngOnInit() {
+    this.userService.getUser().subscribe((user) => {
+        this.user = user;
+
+        this.menuInit();
+    });
+
+    $.expr[":"].regex = function(elem, index, match) {
+      const matchParams = match[3].split(","),
+          validLabels = /^(data|css):/,
+          attr = {
+              method: matchParams[0].match(validLabels) ? matchParams[0].split(":")[0] : "attr",
+              property: matchParams.shift().replace(validLabels, "")
+          },
+          regexFlags = "ig",
+          regex = new RegExp(matchParams.join("").replace(/^\s+|\s+$/g, ""), regexFlags);
+      return regex.test($(elem)[attr.method](attr.property));
+    };
+  }
+
+  menuInit() {
+    const menus = [
+        {name: "ניהול", link: "/management", permission: USER_PERMISSIONS.DELIVERY},
+        {name: "משתמשים", link: "/management/users", permission: USER_PERMISSIONS.ADMIN},
+        {name: "קריאות", link: "/management/support", permission: USER_PERMISSIONS.REPRESENTATIVE},
+        {name: "משימות", link: "/missions", permission: USER_PERMISSIONS.DELIVERY},
+        {name: "אנשי קשר", link: "/contacts", permission: USER_PERMISSIONS.LOGISTICS}
+        // {name: "סטטיסטיקות", link: "/management/statistics", permission: USER_PERMISSIONS.MANAGER},
+        // {name: "צפיה בצ'אטים", link: "/management/chatSupervise", permission: USER_PERMISSIONS.MANAGER},
+    ];
+
+    menus.forEach(menu => {
+        if (this.user.permissions <= menu.permission) {
+            this.menus.push(menu);
+        }
+    });
+  }
+>>>>>>> parent of 1a7cf91... add user details
 }
