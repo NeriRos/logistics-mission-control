1a7cf9129053408028e0f13d5fd08adf8c9fdfc0
neriyarosner <neriyarosner@gmail.com>
2018-10-31 13:04:26 +0200

    add user details

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -3,53 +3,77 @@ import { UserService } from "./services/login.service";
 import { IUser, USER_PERMISSIONS } from "./models/user.model";
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-  user: IUser;
-  menus: Array<{name: string, link: string, permission: number}> = [];
+    user: IUser;
+    menus: Array<{name: string, link: string, permission: number}> = [];
+    greeting: String = "";
+    REFRESH_HELLO_MESSAGE_MINUTES = 1;
 
-  constructor(private userService: UserService) {
+    constructor(private userService: UserService) {
+        this.setGreeting();
+    }
 
-  }
+    ngOnInit() {
+        this.userService.getUser().subscribe((user) => {
+            this.user = user;
 
-  ngOnInit() {
-    this.userService.getUser().subscribe((user) => {
-        this.user = user;
+            this.menuInit();
+        });
 
-        this.menuInit();
-    });
+        $.expr[":"].regex = function(elem, index, match) {
+        const matchParams = match[3].split(","),
+            validLabels = /^(data|css):/,
+            attr = {
+                method: matchParams[0].match(validLabels) ? matchParams[0].split(":")[0] : "attr",
+                property: matchParams.shift().replace(validLabels, "")
+            },
+            regexFlags = "ig",
+            regex = new RegExp(matchParams.join("").replace(/^\s+|\s+$/g, ""), regexFlags);
+        return regex.test($(elem)[attr.method](attr.property));
+        };
+    }
 
-    $.expr[":"].regex = function(elem, index, match) {
-      const matchParams = match[3].split(","),
-          validLabels = /^(data|css):/,
-          attr = {
-              method: matchParams[0].match(validLabels) ? matchParams[0].split(":")[0] : "attr",
-              property: matchParams.shift().replace(validLabels, "")
-          },
-          regexFlags = "ig",
-          regex = new RegExp(matchParams.join("").replace(/^\s+|\s+$/g, ""), regexFlags);
-      return regex.test($(elem)[attr.method](attr.property));
-    };
-  }
+    setGreeting() {
+        const greet = () => {
+            const hour = (new Date()).getHours();
 
-  menuInit() {
-    const menus = [
-        {name: "ניהול", link: "/management", permission: USER_PERMISSIONS.DELIVERY},
-        {name: "משתמשים", link: "/management/users", permission: USER_PERMISSIONS.ADMIN},
-        {name: "קריאות", link: "/management/support", permission: USER_PERMISSIONS.REPRESENTATIVE},
-        {name: "משימות", link: "/missions", permission: USER_PERMISSIONS.DELIVERY},
-        {name: "אנשי קשר", link: "/contacts", permission: USER_PERMISSIONS.LOGISTICS}
-        // {name: "סטטיסטיקות", link: "/management/statistics", permission: USER_PERMISSIONS.MANAGER},
-        // {name: "צפיה בצ'אטים", link: "/management/chatSupervise", permission: USER_PERMISSIONS.MANAGER},
-    ];
+            if (hour >= 6 && hour <= 12) {
+                this.greeting = "בוקר טוב,";
+            } else if (hour >= 12 && hour <= 18) {
+                this.greeting = "צהריים טובים,";
+            } else if (hour >= 18 && hour <= 24) {
+                this.greeting = "ערב טוב,";
+            } else if (hour >= 24 && hour <= 6) {
+                this.greeting = "לילה טוב,";
+            }
 
-    menus.forEach(menu => {
-        if (this.user.permissions <= menu.permission) {
-            this.menus.push(menu);
-        }
-    });
-  }
+            return this.greeting;
+        };
+
+        greet();
+
+        setInterval(greet, 1000 * 60 * this.REFRESH_HELLO_MESSAGE_MINUTES);
+    }
+
+    menuInit() {
+        const menus = [
+            {name: "ניהול", link: "/management", permission: USER_PERMISSIONS.DELIVERY},
+            {name: "משתמשים", link: "/management/users", permission: USER_PERMISSIONS.ADMIN},
+            {name: "קריאות", link: "/management/support", permission: USER_PERMISSIONS.REPRESENTATIVE},
+            {name: "משימות", link: "/missions", permission: USER_PERMISSIONS.DELIVERY},
+            {name: "אנשי קשר", link: "/contacts", permission: USER_PERMISSIONS.LOGISTICS}
+            // {name: "סטטיסטיקות", link: "/management/statistics", permission: USER_PERMISSIONS.MANAGER},
+            // {name: "צפיה בצ'אטים", link: "/management/chatSupervise", permission: USER_PERMISSIONS.MANAGER},
+        ];
+
+        menus.forEach(menu => {
+            if (this.user.permissions <= menu.permission) {
+                this.menus.push(menu);
+            }
+        });
+    }
 }
